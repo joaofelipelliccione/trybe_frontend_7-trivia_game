addbf218d0e8f415a6845cfe2cb80d72d5149a4c
João Felipe P <jf.pelliccione@gmail.com>
2021-11-29 17:46:00 -0300

    Updates

---
+++ precache-manifest.897807c3be3ec830f8e9dd32c6f1ca98.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f8f18d6a5d51897e5d4a3b6501a9be8",
+    "url": "/trybe_frontend_7-trivia_game/index.html"
+  },
+  {
+    "revision": "f9b1405a0e49fb6c5fae",
+    "url": "/trybe_frontend_7-trivia_game/static/css/main.e8ed5596.chunk.css"
+  },
+  {
+    "revision": "39929ec4eaa8249379b5",
+    "url": "/trybe_frontend_7-trivia_game/static/js/2.19d8cb72.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trybe_frontend_7-trivia_game/static/js/2.19d8cb72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9b1405a0e49fb6c5fae",
+    "url": "/trybe_frontend_7-trivia_game/static/js/main.9d97221a.chunk.js"
+  },
+  {
+    "revision": "4878d2feb58e40d7905c",
+    "url": "/trybe_frontend_7-trivia_game/static/js/runtime-main.c0a24a24.js"
+  },
+  {
+    "revision": "10a45c728ab0817a78f55635d9515bd6",
+    "url": "/trybe_frontend_7-trivia_game/static/media/feedback-game.10a45c72.svg"
+  },
+  {
+    "revision": "ceccd1dced56bf6ae1ab46570138939d",
+    "url": "/trybe_frontend_7-trivia_game/static/media/feedback-reviews.ceccd1dc.svg"
+  },
+  {
+    "revision": "049d53928556a9632cc47fb77bc0738b",
+    "url": "/trybe_frontend_7-trivia_game/static/media/settings-img.049d5392.svg"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trybe_frontend_7-trivia_game/static/media/trivia.466d153e.png"
+  }
+]);
